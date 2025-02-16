87b64673f53ff57ae0cc3d814f513f3a5686e440
Nafanias <vag57205@gmail.com>
2025-02-16 23:24:51 +0300

    Updates

---
+++ _app/immutable/nodes/6.B72Lk_Sj.js
@@ -0,0 +1,132 @@
+import"../chunks/Bg9kRutz.js";import"../chunks/69_IOA4Y.js";import{T as e,U as P,J as v,V as s,K as u}from"../chunks/DwB3qp1y.js";import{t as f,a as r,b as a}from"../chunks/DhYkO-ht.js";import{O as _}from"../chunks/DKnAL-hi.js";import{C as p}from"../chunks/CGardy9S.js";import{I as B}from"../chunks/DnjDcRZx.js";import{O as jt}from"../chunks/DkdKJtbA.js";import{U as I}from"../chunks/B8uNzu4-.js";import{D as Nt}from"../chunks/LYX3zNua.js";import{L as d}from"../chunks/DF4CoHH5.js";var Jt=f("В приведённом выше примере мы использовали функции open() и close() для открытия и закрытия JSON-файла. Если вы не знакомы с обработкой файлов в Python, пожалуйста, ознакомьтесь с разделом <!>.",1),Ot=f("Для получения дополнительной информации о чтении JSON-файлов обратитесь к разделу <!>",1),St=f("<!> <!> <!>",1),wt=f("<!> <!> <!> <!> <!> <!> <!> <!> <!>",1),xt=f('<span class="bold svelte-oir1bt">dictionary:</span> имя словаря, который должен быть преобразован в JSON-объект.',1),kt=f('<span class="bold svelte-oir1bt">indent:</span> определяет количество единиц для отступа.',1),Ct=f("<!> <!>",1),Ft=f('<span class="bold svelte-oir1bt">dictionary:</span> имя словаря, который должен быть преобразован в JSON-объект.',1),Gt=f('<span class="bold svelte-oir1bt">file pointer:</span> указатель на файл, открытый в режиме записи или добавления.',1),Vt=f("<!> <!>",1),Dt=f('<h1 class="svelte-oir1bt">Json в Python</h1> <p class="svelte-oir1bt">Примечание: в Python данные JSON обычно представлены в виде строки.</p> <h2 id="importPython" class="svelte-oir1bt">Импортирование модуля</h2> <p class="svelte-oir1bt">Чтобы использовать любой модуль в Python, всегда необходимо импортировать этот модуль. Мы можем импортировать модуль json, используя оператор <!>.</p> <p class="svelte-oir1bt">Пример: Импортирование модуля JSON.</p> <!> <h2 id="parsingPython" class="svelte-oir1bt">Парсинг JSON – Конвертация из JSON в Python</h2> <p class="svelte-oir1bt">Функции <!> и <!> модуля json упрощают парсинг JSON-объектов.</p> <h2 class="svelte-oir1bt">Парсинг JSON-строки</h2> <p class="svelte-oir1bt">Метод <!> используется для парсинга JSON-строк в Python, и результатом будет словарь Python.</p> <p class="svelte-oir1bt">Синтаксис:</p> <!> <p class="svelte-oir1bt"><span class="bold svelte-oir1bt">Пример:</span>Преобразование JSON в словарь</p> <!> <p class="svelte-oir1bt">Результат</p> <!> <p class="svelte-oir1bt"><span class="bold svelte-oir1bt">Примечание:</span>Для получения более подробной информации <!></p> <h2 id="readFilePython" class="svelte-oir1bt">Чтение JSON-файла</h2> <p class="svelte-oir1bt"><!>. Метод может прочитать файл, который содержит JSON-объект. Предположим, у вас есть файл с именем student.json, содержащий данные о студенте, и мы хотим прочитать этот файл.</p> <p class="svelte-oir1bt">Синтаксис:</p> <!> <p class="svelte-oir1bt"><span class="bold svelte-oir1bt">Пример:</span>Чтение JSON-файла с использованием Python.</p> <p class="svelte-oir1bt">Предположим, файл выглядит следующим образом.</p> <div style="text-align: center; margin-top: 20px;"><div style="width: 60%; display: inline-block;"><!></div></div> <p class="svelte-oir1bt"><span class="bold svelte-oir1bt">Пример:</span>Преобразование JSON в словарь</p> <!> <p class="svelte-oir1bt">Результат</p> <div style="text-align: center; margin-top: 20px;"><div style="width: 60%; display: inline-block;"><!></div></div> <span class="bold svelte-oir1bt">Примечание</span> <!> <h2 id="convertPythonJSON" class="svelte-oir1bt">Преобразование из Python в JSON</h2> <p class="svelte-oir1bt">Методы <!> и <!> модуля json могут быть использованы для преобразования объектов Python в JSON.</p> <p class="svelte-oir1bt">Следующие типы объектов Python могут быть преобразованы в JSON-строки:</p> <!> <p class="svelte-oir1bt">Объекты Python и их эквивалентные преобразования в JSON:</p> <!> <h2 id="convertPythonString" class="svelte-oir1bt">Преобразование в JSON-строку</h2> <p class="svelte-oir1bt">Метод <!> может преобразовать объект Python в JSON-строку.</p> <p class="svelte-oir1bt">Синтаксис:</p> <!> <p class="svelte-oir1bt">Он принимает два параметра:</p> <!> <p class="svelte-oir1bt"><span class="bold svelte-oir1bt">Пример:</span>Преобразование словаря Python в JSON-строку</p> <!> <p class="svelte-oir1bt">Результат</p> <!> <h2 id="writingPythonJsonFile" class="svelte-oir1bt">Запись в JSON-файл</h2> <p class="svelte-oir1bt">Метод <!> может быть использован для записи в JSON-файл.</p> <p class="svelte-oir1bt">Синтаксис:</p> <!> <p class="svelte-oir1bt">Он принимает два параметра:</p> <!> <p class="svelte-oir1bt"><span class="bold svelte-oir1bt">Пример:</span> Запись в JSON-файл.</p> <!> <p class="svelte-oir1bt">Результат</p> <div style="text-align: center; margin-top: 20px;"><div style="width: 60%; display: inline-block;"><!></div></div> <h2 id="FormattingPython" class="svelte-oir1bt">Форматирование JSON</h2> <p class="svelte-oir1bt">В приведенном выше примере вы, должно быть, заметили, что при конвертации объекта Python в JSON он не форматируется, а вывод идет по прямой линии. Мы можем отформатировать JSON, передав параметр indent методу dumps().</p> <p class="svelte-oir1bt"><span class="bold svelte-oir1bt">Пример:</span>форматирование JSON</p> <!> <p class="svelte-oir1bt"><span class="bold svelte-oir1bt">Вывод</span></p> <!> <h2 id="sortPython" class="svelte-oir1bt">Сортировка JSON</h2> <p class="svelte-oir1bt">Мы можем сортировать данные JSON с помощью параметра sort_keys метода dumps(). Этот параметр принимает логическое значение и возвращает отсортированный JSON, если переданное значение равно True. По умолчанию переданное значение равно False.</p> <p class="svelte-oir1bt"><span class="bold svelte-oir1bt">Пример:</span>сортировка JSON</p> <!> <p class="svelte-oir1bt"><span class="bold svelte-oir1bt">Вывод</span></p> <!>',1);function Mt(pt){var T=Dt(),J=e(P(T),6),vt=e(v(J));_(vt,{visited:!0,href:"https://www.geeksforgeeks.org/import-module-python/",children:(o,l)=>{s();var t=a("import");r(o,t)},$$slots:{default:!0}}),s(),u(J);var R=e(J,4);p(R,{type:"multi",children:(o,l)=>{s();var t=a();t.nodeValue=`# importing json module
+	import json`,r(o,t)},$$slots:{default:!0}});var O=e(R,4),z=e(v(O));_(z,{visited:!0,href:"https://www.geeksforgeeks.org/json-load-in-python/",children:(o,l)=>{s();var t=a("load()");r(o,t)},$$slots:{default:!0}});var ut=e(z,2);_(ut,{visited:!0,href:"https://www.geeksforgeeks.org/json-loads-in-python/",children:(o,l)=>{s();var t=a("loads()");r(o,t)},$$slots:{default:!0}}),s(),u(O);var S=e(O,4),ct=e(v(S));_(ct,{visited:!0,href:"https://www.geeksforgeeks.org/json-loads-in-python/",children:(o,l)=>{s();var t=a("loads()");r(o,t)},$$slots:{default:!0}}),s(),u(S);var q=e(S,4);p(q,{hideCopyButton:!0,code:"json.loads(json_string)"});var H=e(q,4);p(H,{type:"multi",children:(o,l)=>{s();var t=a();t.nodeValue=`# Python program to convert JSON to Dict 
+import json
+ 
+# JSON string
+employee ='{"name": "Nitin", "department":"Finance","company":"GFG"}'
+ 
+# Convert string to Python dict
+employee_dict = json.loads(employee)
+print("Data after conversion")
+print(employee_dict)
+print(employee_dict['department'])
+ 
+print("
+Type of data")
+print(type(employee_dict))`,r(o,t)},$$slots:{default:!0}});var U=e(H,4);p(U,{type:"multi",hideCopyButton:!0,showMoreLess:!1,children:(o,l)=>{s();var t=a();t.nodeValue=`Данные после конвертации
+{'имя': 'Нитин', 'отдел': 'Финансы', 'компания': 'GFG'}
+Финансы
+
+Тип данных
+<класс 'dict'>`,r(o,t)},$$slots:{default:!0}});var w=e(U,2),ht=e(v(w),2);_(ht,{visited:!0,href:"https://www.geeksforgeeks.org/how-to-parse-data-from-json-into-python/",children:(o,l)=>{s();var t=a("Parse Data From JSON into Python");r(o,t)},$$slots:{default:!0}}),u(w);var x=e(w,4),$t=v(x);_($t,{visited:!0,href:"https://www.geeksforgeeks.org/json-load-in-python/",children:(o,l)=>{s();var t=a("load()");r(o,t)},$$slots:{default:!0}}),s(),u(x);var E=e(x,4);p(E,{hideCopyButton:!0,code:"json.load(file_object)"});var k=e(E,6),K=v(k),_t=v(K);B(_t,{src:"https://media.geeksforgeeks.org/wp-content/uploads/20210727210403/readjsonPython.png",style:"width: 100%;"}),u(K),u(k);var M=e(k,4);p(M,{type:"multi",children:(o,l)=>{s();var t=a();t.nodeValue=`# Python program to read
+# json file
+ 
+ 
+import json
+ 
+# Opening JSON file
+f = open('data.json',)
+ 
+# returns JSON object as
+# a dictionary
+data = json.load(f)
+ 
+# Iterating through the json
+# list
+for i in data:
+    print(i)
+ 
+# Closing file
+f.close()`,r(o,t)},$$slots:{default:!0}});var C=e(M,4),A=v(C),mt=v(A);B(mt,{src:"https://media.geeksforgeeks.org/wp-content/uploads/20210727210608/Pythonreadjson.png",style:"width: 100%;"}),u(A),u(C);var Q=e(C,4);jt(Q,{style:"margin-left: 3.5em;",children:(o,l)=>{var t=St(),m=P(t);d(m,{children:($,c)=>{s();var y=a("Данные JSON преобразуются в список словарей на Python.");r($,y)},$$slots:{default:!0}});var g=e(m,2);d(g,{children:($,c)=>{s();var y=Jt(),j=e(P(y));_(j,{visited:!0,href:"https://www.geeksforgeeks.org/file-handling-python/",children:(N,L)=>{s();var n=a("File Handling in Python");r(N,n)},$$slots:{default:!0}}),s(),r($,y)},$$slots:{default:!0}});var h=e(g,2);d(h,{children:($,c)=>{s();var y=Ot(),j=e(P(y));_(j,{visited:!0,href:"https://www.geeksforgeeks.org/read-json-file-using-python/",children:(N,L)=>{s();var n=a("Read JSON file using Python");r(N,n)},$$slots:{default:!0}}),r($,y)},$$slots:{default:!0}}),r(o,t)},$$slots:{default:!0}});var F=e(Q,4),W=e(v(F));_(W,{visited:!0,href:"https://www.geeksforgeeks.org/json-dump-in-python/",children:(o,l)=>{s();var t=a("dump()");r(o,t)},$$slots:{default:!0}});var ft=e(W,2);_(ft,{visited:!0,href:"https://www.geeksforgeeks.org/json-dumps-in-python/",children:(o,l)=>{s();var t=a("dumps()");r(o,t)},$$slots:{default:!0}}),s(),u(F);var X=e(F,4);I(X,{style:"margin-left: 3.5em;",children:(o,l)=>{var t=wt(),m=P(t);d(m,{children:(n,b)=>{s();var i=a("dict (словарь)");r(n,i)},$$slots:{default:!0}});var g=e(m,2);d(g,{children:(n,b)=>{s();var i=a("list (список)");r(n,i)},$$slots:{default:!0}});var h=e(g,2);d(h,{children:(n,b)=>{s();var i=a("tuple (кортеж)");r(n,i)},$$slots:{default:!0}});var $=e(h,2);d($,{children:(n,b)=>{s();var i=a("string (строка)");r(n,i)},$$slots:{default:!0}});var c=e($,2);d(c,{children:(n,b)=>{s();var i=a("int (целое число)");r(n,i)},$$slots:{default:!0}});var y=e(c,2);d(y,{children:(n,b)=>{s();var i=a("float (число с плавающей точкой)");r(n,i)},$$slots:{default:!0}});var j=e(y,2);d(j,{children:(n,b)=>{s();var i=a("True (истина)");r(n,i)},$$slots:{default:!0}});var N=e(j,2);d(N,{children:(n,b)=>{s();var i=a("False (ложь)");r(n,i)},$$slots:{default:!0}});var L=e(N,2);d(L,{children:(n,b)=>{s();var i=a("None (ничто)");r(n,i)},$$slots:{default:!0}}),r(o,t)},$$slots:{default:!0}});var Y=e(X,4);Nt(Y,{zebra:!0,headers:[{key:"name",value:"Python "},{key:"protocol",value:"JSON Equivalent"}],rows:[{id:"a",name:"dict",protocol:"object"},{id:"b",name:"list, tuple",protocol:"array"},{id:"c",name:"str",protocol:"string"},{id:"d",name:"int, float",protocol:"number"},{id:"e",name:"True",protocol:"true"},{id:"f",name:"False",protocol:"false"},{id:"g",name:"None",protocol:"null"}]});var G=e(Y,4),yt=e(v(G));_(yt,{visited:!0,href:"https://www.geeksforgeeks.org/json-dumps-in-python/",children:(o,l)=>{s();var t=a("dumps()");r(o,t)},$$slots:{default:!0}}),s(),u(G);var Z=e(G,4);p(Z,{hideCopyButton:!0,code:"json.dumps(dict, indent)"});var tt=e(Z,4);I(tt,{style:"margin-left: 3.5em;",children:(o,l)=>{var t=Ct(),m=P(t);d(m,{children:(h,$)=>{var c=xt();s(),r(h,c)},$$slots:{default:!0}});var g=e(m,2);d(g,{children:(h,$)=>{var c=kt();s(),r(h,c)},$$slots:{default:!0}}),r(o,t)},$$slots:{default:!0}});var et=e(tt,4);p(et,{type:"multi",children:(o,l)=>{s();var t=a();t.nodeValue=`# Python program to convert
+# Python to JSON
+ 
+ 
+import json
+ 
+# Data to be written
+dictionary = {
+    "name": "sunil",
+    "department": "HR",
+    "Company": 'GFG'
+}
+ 
+# Serializing json
+json_object = json.dumps(dictionary)
+print(json_object)`,r(o,t)},$$slots:{default:!0}});var ot=e(et,4);p(ot,{hideCopyButton:!0,code:'{"name": "sunil", "department": "HR", "Company": "GFG"}'});var V=e(ot,4),gt=e(v(V));_(gt,{visited:!0,href:"https://www.geeksforgeeks.org/json-dumps-in-python/",children:(o,l)=>{s();var t=a("dumps()");r(o,t)},$$slots:{default:!0}}),s(),u(V);var st=e(V,4);p(st,{hideCopyButton:!0,code:"json.dump(dict, file_pointer)"});var rt=e(st,4);I(rt,{style:"margin-left: 3.5em;",children:(o,l)=>{var t=Vt(),m=P(t);d(m,{children:(h,$)=>{var c=Ft();s(),r(h,c)},$$slots:{default:!0}});var g=e(m,2);d(g,{children:(h,$)=>{var c=Gt();s(),r(h,c)},$$slots:{default:!0}}),r(o,t)},$$slots:{default:!0}});var at=e(rt,4);p(at,{type:"multi",children:(o,l)=>{s();var t=a();t.nodeValue=`# Python program to write JSON
+# to a file
+ 
+ 
+import json
+ 
+# Data to be written
+dictionary ={
+    "name" : "Nisha",
+    "rollno" : 420,
+    "cgpa" : 10.10,
+    "phonenumber" : "1234567890"
+}
+ 
+with open("sample.json", "w") as outfile:
+    json.dump(dictionary, outfile)`,r(o,t)},$$slots:{default:!0}});var D=e(at,4),lt=v(D),bt=v(lt);B(bt,{src:"https://media.geeksforgeeks.org/wp-content/uploads/20210727221538/Pythonwritejsontofile.png",style:"width: 100%;"}),u(lt),u(D);var nt=e(D,8);p(nt,{type:"multi",children:(o,l)=>{s();var t=a();t.nodeValue=`# Import required libraries
+import json
+ 
+# Initialize JSON data
+json_data = '[ {"studentid": 1, "name": "Nikhil", "subjects": ["Python", "Data Structures"]},{"studentid": 2, "name": "Nisha", "subjects": ["Java", "C++", "R Lang"]} ]'
+ 
+# Create Python object from JSON string 
+# data
+data = json.loads(json_data)
+ 
+# Pretty Print JSON
+json_formatted_str = json.dumps(data, indent=4)
+print(json_formatted_str)`,r(o,t)},$$slots:{default:!0}});var it=e(nt,4);p(it,{type:"multi",hideCopyButton:!0,children:(o,l)=>{s();var t=a();t.nodeValue=`[
+    {
+        "studentid": 1,
+        "имя": "Нихил",
+        "субъекты": [
+            "Питон",
+            «Структуры данных»
+        ]
+    },
+    {
+        "studentid": 2,
+        "имя": "Ниша",
+        "субъекты": [
+            "Ява",
+            "С++",
+            "Р Ланг"
+        ]
+    }
+]`,r(o,t)},$$slots:{default:!0}});var dt=e(it,8);p(dt,{type:"multi",children:(o,l)=>{s();var t=a();t.nodeValue=`# Import required libraries
+import json
+ 
+# Initialize JSON data
+json_data = '[ {"studentid": 1, "name": "Nikhil", "subjects":["Python", "Data Structures"], "company":"GFG"},{"studentid": 2, "name": "Nisha", "subjects":["Java", "C++", "R Lang"], "company":"GFG"} ]'
+ 
+# Create Python object from JSON string 
+# data
+data = json.loads(json_data)
+ 
+# Pretty Print JSON
+json_formatted_str = json.dumps(data, indent=4, sort_keys=True)
+print(json_formatted_str)`,r(o,t)},$$slots:{default:!0}});var Pt=e(dt,4);p(Pt,{type:"multi",hideCopyButton:!0,children:(o,l)=>{s();var t=a();t.nodeValue=`[
+    {
+        "компания": "GFG",
+        "имя": "Нихил",
+        "studentid": 1,
+        "субъекты": [
+            "Питон",
+            «Структуры данных»
+        ]
+    },
+    {
+        "компания": "GFG",
+        "имя": "Ниша",
+        "studentid": 2,
+        "субъекты": [
+            "Ява",
+            "С++",
+            "Р Ланг"
+        ]
+    }
+]`,r(o,t)},$$slots:{default:!0}}),r(pt,T)}export{Mt as component};
