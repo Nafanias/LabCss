1cb77dd56695b121cdea1ffc3e03faaa3dab22c8
Nafanias <vag57205@gmail.com>
2025-02-16 22:57:52 +0300

    Updates

---
+++ _app/immutable/chunks/Bvsy3D1R.js
@@ -0,0 +1,1 @@
+var s;const a=((s=globalThis.__sveltekit_1itx2nm)==null?void 0:s.base)??"/LabCss";var t;const e=((t=globalThis.__sveltekit_1itx2nm)==null?void 0:t.assets)??a;export{e as a,a as b};
--- _app/immutable/entry/start.Bh2bAvs2.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"../chunks/DvY0cVDM.js";export{t as start};
